0bf3171439b8376b004cc00ae20b02b66ddc7fae
plungerman <plungerman@gmail.com>
2021-11-02 11:52:12 -0500

    updates for production

--- djchekhov/wsgi_default.py
+++ djchekhov/wsgi_default.py
@@ -15,5 +15,14 @@ sys.path.append('/d2/python_venv/3.8/djchekhov/lib/python3.8/site-packages/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djchekhov.settings.production')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
+# informix
+os.environ['INFORMIXSERVER'] = ''
+os.environ['DBSERVERNAME'] = ''
+os.environ['INFORMIXDIR'] = ''
+os.environ['ODBCINI'] = ''
+os.environ['ONCONFIG'] = ''
+os.environ['INFORMIXSQLHOSTS'] = ''
+os.environ['LD_LIBRARY_PATH'] = ''
+os.environ['LD_RUN_PATH'] = os.environ['LD_LIBRARY_PATH']
 # wsgi
 application = get_wsgi_application()
